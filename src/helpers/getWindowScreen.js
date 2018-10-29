b47c88513f09571365d5456190d915c0eef90d86
grzegorz <grzegorz@e-gaming.cz>
2018-10-29 09:48:04 +0100

    better match current window screen (then window is outside last screen)

--- src/helpers/getWindowScreen.js
+++ src/helpers/getWindowScreen.js
@@ -1,10 +1,21 @@
 function getWindowScreen(window, screens) {
   const middlePoint = window.width / 2 + window.x
-  return screens.find(s =>
+  const screen = screens.find(s =>
     // console.log(s.x + ' < ' + middlePoint + ' < ' + (s.x + s.width)) ||
-    s.x < middlePoint &&
-    middlePoint < s.x + s.width
+    s.x <= middlePoint &&
+    middlePoint <= (s.x + s.width)
   )
+
+  if (!screen) {
+    const lastScreen = screens[screens.length - 1]
+    if (middlePoint > lastScreens.x + s.width) {
+      return lastScreen
+    }
+
+    return screens[0]
+  }
+
+  return screen
 }
 
 module.exports = getWindowScreen;
